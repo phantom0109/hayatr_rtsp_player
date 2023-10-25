0db7483443e57742a180c63d933de715a183d582
Kimura Hayate <hayatekimura0109@gmail.com>
2023-10-25 23:52:30 +0900

    a.483 ticket

--- pages/posts/123.js
+++ pages/posts/123.js
@@ -1,5 +1,3 @@
-import React from 'react';
-import ReactPlayer from 'react-player';
 import { useMemo } from 'react';
 import MuxPlayer from '@mux/mux-player-react';
 import { getMDXComponent } from 'mdx-bundler/client';
@@ -26,21 +24,14 @@ export async function getStaticPaths() {
 
 export default function BlogPost({ code, frontmatter }) {
   const Component = useMemo(() => getMDXComponent(code), [code]);
-  const rtspUrl = "rtsp://ineyt:jio12@110.39.5.211:554/cam/realmonitor?channel=2&subtype=0";
 
   return (
     <>
-    
       <h1>{frontmatter.title}</h1>
       <article>
-      <div>
-        <ReactPlayer url={rtspUrl} controls={true} width="100%" height="auto" />
-        {/* <ReactPlayer url="#" controls={true} width="100%" height="auto" /> */}
-      </div>
         <Component
           components={{
             MuxPlayer,
-            // Video
           }}
         />
       </article>
